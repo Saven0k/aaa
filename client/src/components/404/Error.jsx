3537f2b8848cb75d546816fc308ae66de987e314
Savenok <Romasav2017@gmail.com>
2025-03-22 22:54:03 +0300

    404 last version

--- client/src/components/404/Error.jsx
+++ client/src/components/404/Error.jsx
@@ -1,13 +1,16 @@
 import './style.css'
+import { Link } from "react-router-dom"
 
 const Error = () => {
-
     return (
-        <div className='all'>
+        <div className='all center'>
 
             <div className="er">
                 <h1 className="h1">404</h1>
                 <h1 className='h2'>Ошибочка......</h1>
+                <Link  to={'/'}>
+                    <h3 className='h3'>На главную</h3>
+                </Link>
             </div>
         </div>
     )
